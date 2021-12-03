de8da7b058200e137523825a640d829ae972d749
Miranda Wilson <miranda@snowplowanalytics.com>
2021-12-03 17:08:59 +0000

    Finish day 1

--- day1/src/main/java/day1.java
+++ day1/src/main/java/day1.java
@@ -6,19 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class day1 {
-    public static List<Integer> fileContents = new ArrayList<>();
+public class Day1 {
+    public List<Integer> fileContents = new ArrayList<>();
+    public Integer increaseCount = 0;
 
-    public static void fileDataToList(String pathString) {
+    public void fileDataToList(String pathString) {
         Path path = Paths.get(pathString);
         try (Stream<String> lines = Files.lines(path)) {
-
             lines.forEach(line -> fileContents.add(Integer.parseInt(line)));
-
         } catch (IOException exception) {
-            System.out.println("why didn't it work?");
+            System.out.println("it didn't work?");
         }
     }
 
+    public void countNumberIncreases(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if ((numbers.get(i)) < numbers.get(i + 1)) {
+                increaseCount += 1;
+            }
+        }
+    }
 
+    public void countSlidingWindowIncreases(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 3; i++) {
+            int currentTotal = numbers.get(i) + numbers.get(i + 1) + numbers.get(i + 2);
+            int nextTotal = numbers.get(i + 1) + numbers.get(i + 2) + numbers.get(i + 3);
+
+            if (currentTotal < nextTotal) {
+                increaseCount += 1;
+            }
+        }
+    }
 }
--- day1/src/test/java/day1Test.java
+++ day1/src/test/java/day1Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -14,12 +15,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class day1Test {
 
+    Day1 day1;
+
+    @BeforeEach
+    void setUp() {
+        day1 = new Day1();
+    }
+
     @Test
     public void fileDataExtractedAsExpected() {
         List<Integer> expected = Arrays.asList(199, 200, 208, 210);
-
-        day1.fileDataToList("./src/main/resources/exampleData.txt");
-
+        day1.fileDataToList("./src/main/resources/exampleDataPt1.txt");
         assertEquals(String.valueOf(expected), String.valueOf(day1.fileContents.subList(0, 4)));
+    }
+
+    @Test
+    public void nextNumberIsHigherWindowSize1() {
+        List<Integer> numbers = Arrays.asList(1, 2);
+        day1.countNumberIncreases(numbers);
+        assertEquals(1, day1.increaseCount);
+    }
+
+    @Test
+    public void nextNumberIsLowerWindowSize1() {
+        List<Integer> numbers = Arrays.asList(10, 3);
+        day1.countNumberIncreases(numbers);
+        assertEquals(0, day1.increaseCount);
+    }
+
+    @Test
+    public void rightAnswerForExampleDataWindowSize1() {
+        day1.fileDataToList("./src/main/resources/exampleDataPt1.txt");
+        List<Integer> numbers = day1.fileContents;
+        day1.countNumberIncreases(numbers);
+        assertEquals(7, day1.increaseCount);
+    }
+
+    @Test
+    public void nextWindowIsHigher() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        day1.countSlidingWindowIncreases(numbers);
+        assertEquals(1, day1.increaseCount);
+    }
+
+    @Test
+    public void nextWindowIsTheSame() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 1);
+        day1.countSlidingWindowIncreases(numbers);
+        assertEquals(0, day1.increaseCount);
+    }
+
+    @Test
+    public void rightAnswerForExamplePart2() {
+        day1.fileDataToList("./src/main/resources/exampleDataPt2.txt");
+        List<Integer> numbers = day1.fileContents;
+        day1.countSlidingWindowIncreases(numbers);
+        assertEquals(5, day1.increaseCount);
     }
 }
